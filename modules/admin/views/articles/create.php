9d970b9bbd4bdef687c04d74ed2be86c211201d1
hiscaler <hiscaler@gmail.com>
2017-12-01 19:02:18 +0800

    admin: ...

--- modules/admin/views/articles/create.php
+++ modules/admin/views/articles/create.php
@@ -2,7 +2,7 @@
 /* @var $this yii\web\View */
 /* @var $model app\models\Article */
 
-$this->title = Yii::t('app', 'Create Article');
+$this->title = Yii::t('app', 'Create');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Articles'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 
