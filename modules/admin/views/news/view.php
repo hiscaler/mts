34b20402b0c17c4749f8e1d3a5348ffb2b980a9e
hiscaler <hiscaler@gmail.com>
2018-01-07 12:30:45 +0800

    admin: ...

--- modules/admin/views/news/view.php
+++ modules/admin/views/news/view.php
@@ -5,7 +5,7 @@ use yii\helpers\Inflector;
 use yii\widgets\DetailView;
 
 /* @var $this yii\web\View */
-/* @var $model common\models\News */
+/* @var $model app\models\News */
 
 $this->title = $model->title;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'News'), 'url' => ['index']];
